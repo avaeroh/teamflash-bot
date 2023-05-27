b30fac0eaedbf81bd05d1942978a991f9dd5bcf7
Avaeroh <jonathan.mh@hotmail.co.uk>
2023-05-27 10:43:39 +0100

    arm-based support
    arm-based support
    
    arm-based support
    
    arm-based support
    
    arm-based support
    
    arm-based support
    
    arm-based support
    
    arm-based support
    
    arm-based support
    
    arm-based support
    
    arm-based support
    
    arm-based support
    
    arm-based support

--- src/database/connectDatabase.ts
+++ src/database/connectDatabase.ts
@@ -1,7 +1,16 @@
 import { connect } from 'mongoose';
+import { type } from 'os';
 
 export const connectDatabase = async () => {
   console.log('Connecting to database...');
-  await connect(process.env.MONGO_URI!);
-  console.log('Database Connected!');
+  await connect(process.env.MONGO_URI!)
+    .then(() => {
+      console.log('Database Connected!');
+    })
+    .catch((error) => {
+      console.error('DB Connection failed:' + error);
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    });
 };
